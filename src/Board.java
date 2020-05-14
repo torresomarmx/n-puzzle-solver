b1ba0f67943094b060339ed98194d69e83ee2e5f
Omar <torresomarmx@gmail.com>
2020-05-14 06:20:32 -0400

    write manhattan distance calculator

--- src/Board.java
+++ src/Board.java
@@ -3,23 +3,38 @@ import java.util.Arrays;
 public class Board {
 
     private int[][] board;
+
+    private int hammingDistance = 0;
+
+    private int manhattanDistance = 0;
     // create a board from an n-by-n array of tiles,
     // where tiles[row][col] = tile at (row, col)
     public Board(int[][] tiles) {
-       this.board = tiles;
+        this.board = tiles;
+       int currentOrderedTile = 1;
+       for (int i = 0; i < this.board.length; i++) {
+           for (int j = 0; j < this.board.length; j++) {
+            if (this.board[i][j] != 0 && this.board[i][j] != currentOrderedTile) {
+                this.hammingDistance += 1;
+                int[] coordinatesForMisplacedTile = this.getCoordinates(this.board[i][j]);
+                this.manhattanDistance += (Math.abs(coordinatesForMisplacedTile[0] - i) +
+                                            Math.abs(coordinatesForMisplacedTile[1] - j));
+            }
+            currentOrderedTile += 1;
+           }
+       }
     }
 
     // string representation of this board
     public String toString() {
         String[] boardString = new String[this.board.length];
         for (int i = 0; i < this.board.length; i++) {
-            String[] currentRow = new String[this.board.length + 1]; // +1 for new line char
+            String[] currentRow = new String[this.board.length];
             for (int j = 0; j < this.board.length; j++)
                currentRow[j] = this.board[i][j] + " ";
-            currentRow[currentRow.length -1] = "\n";
-            boardString[i] = Arrays.toString(currentRow);
+            boardString[i] = String.join(" ", currentRow);
         }
-        return Arrays.toString(boardString);
+        return String.join("\n", boardString);
     }
 
     // tile at (row, col) or 0 if blank
@@ -33,24 +48,50 @@ public class Board {
     }
 
     // number of tiles out of place
-    public int hamming()
+    public int hamming() {
+        return this.hammingDistance;
+    }
 
     // sum of Manhattan distances between tiles and goal
-    public int manhattan()
+    public int manhattan() {
+        return this.manhattanDistance;
+    }
 
     // is this board the goal board?
-    public boolean isGoal()
+    // public boolean isGoal() {}
 
-    // does this board equal y?
-    public boolean equals(Object y)
+    // // does this board equal y?
+    // public boolean equals(Object y) {}
 
-    // all neighboring boards
-    public Iterable<Board> neighbors()
+    // // all neighboring boards
+    // public Iterable<Board> neighbors() {}
 
-    // is this board solvable?
-    public boolean isSolvable()
+    // // is this board solvable?
+    // public boolean isSolvable() {}
+
+    public int[] getCoordinates(int tileNumber) {
+        int row = (tileNumber / this.board.length) - 1;
+        int col = tileNumber % this.board.length;
+        if (col == 0) col = this.board.length - 1;
+        else {
+            row += 1;
+            col = -1 + col;
+        }
+        return new int[]{row, col};
+    }
+
 
     // unit testing (required)
-    public static void main(String[] args)
+    public static void main(String[] args) {
+        int[][] testTiles = new int[3][3];
+        testTiles[0] = new int[]{8,1,3};
+        testTiles[1] = new int[]{4,0,2};
+        testTiles[2] = new int[]{7,6,5};
+        Board testBoard = new Board(testTiles);
+        System.out.println(testBoard);
+        System.out.println(Arrays.toString(testBoard.getCoordinates(13)));
+        System.out.println(testBoard.hamming());
+        System.out.println(testBoard.manhattan());
+    }
 
 }
