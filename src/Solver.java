c703d01609d705d35efa05c38bc95c298321ae28
Omar <torresomarmx@gmail.com>
2020-05-22 14:10:15 -0400

    finish solver class

--- src/Solver.java
+++ src/Solver.java
@@ -1,9 +1,14 @@
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
-import java.util.Arrays;
+import java.util.ArrayDeque;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class Solver {
+
+    private int numberOfMoves = 0;
+
+    private ArrayDeque<Board> boardsFromGoalToStart = new ArrayDeque<>();
 
     private class SearchNode implements Comparable<SearchNode> {
         private Board board;
@@ -51,47 +56,63 @@ public class Solver {
         }
         if (goalNode == null)
             throw new IllegalArgumentException("An error occurred");
-        System.out.println(String.format("Minimum number of moves: %s", goalNode.getNumberOfMoves()));
+        this.numberOfMoves = goalNode.getNumberOfMoves();
         SearchNode currentSearchNodeBackTrace = goalNode;
         while (currentSearchNodeBackTrace != null) {
-            System.out.println(currentSearchNodeBackTrace.getBoard());
-            System.out.println();
+            this.boardsFromGoalToStart.add(currentSearchNodeBackTrace.getBoard());
             currentSearchNodeBackTrace = currentSearchNodeBackTrace.getPreviousNode();
         }
     }
 
     // min number of moves to solve initial board
-//    public int moves()
-//
-//    // sequence of boards in a shortest solution
-//    public Iterable<Board> solution()
-//
+    public int moves() {
+        return this.numberOfMoves;
+    }
+    // sequence of boards in a shortest solution
+    public Iterable<Board> solution() {
+        return new SolutionPathIterable();
+    }
+
+    private class SolutionPathIterable implements Iterable<Board> {
+        @Override
+        public Iterator<Board> iterator() {
+            return new SolutionPathIterator();
+        }
+
+        private class SolutionPathIterator implements Iterator<Board> {
+            @Override
+            public boolean hasNext() {
+                return Solver.this.boardsFromGoalToStart.size() > 0;
+            }
+
+            @Override
+            public Board next() {
+                return Solver.this.boardsFromGoalToStart.removeLast();
+            }
+
+            @Override
+            public void remove() {
+                throw new UnsupportedOperationException();
+            }
+        }
+    }
+
 //    // test client (see below)
     public static void main(String[] args) throws Exception {
         Scanner fileScanner = new Scanner(new BufferedInputStream(new FileInputStream(args[0])));
         int n = fileScanner.nextInt();
-        int[][] testTiles = new int[n][];
-        int[] currentRow = new int[n];
-        int currentRowIdx = 0;
-        int currentColIdx = 0;
-        while (currentRowIdx < n) {
-           if (currentColIdx == n) {
-               testTiles[currentRowIdx] = currentRow;
-               currentColIdx = 0;
-               currentRow = new int[n];
-               currentRowIdx += 1;
-               continue;
-           }
-           currentRow[currentColIdx] = fileScanner.nextInt();
-           currentColIdx += 1;
+        int[][] testTiles = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++)
+                testTiles[i][j] = fileScanner.nextInt();
         }
-        //  System.out.println(n);
-        //  int[][] testTiles = new int[4][];
-        //  testTiles[0] = new int[]{1,2,3,4};
-        //  testTiles[1] = new int[]{5,6,0,8};
-        //  testTiles[2] = new int[]{9,10,7,11};
-        //  testTiles[3] = new int[]{13,14,15,12};
         Board testBoard = new Board(testTiles);
         Solver solver = new Solver(testBoard);
+
+        System.out.println(String.format("Number of moves: %d", solver.moves()));
+        for (Board boardInPath : solver.solution()) {
+            System.out.println(boardInPath);
+            System.out.println();
+        }
     }
 }
